1d31d4b0c0cef8607f3b67874d2f05c53975ff14
prtk2001 <prtk2001@gmail.com>
2021-06-10 17:40:54 +0530

    added new about us

--- src/container/Index/Index.js
+++ src/container/Index/Index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Footer from '../../component/Footer/Footer';
@@ -7,51 +7,66 @@ import Contact from '../Page/Contact/Contact';
 import About from '../Page/About/About';
 import Blog from '../Page/Blog/Blog';
 import Recruitment from '../Page/Recruitment/Recruitment';
+import Events from '../Page/Events/Events';
 
 
 
 
 class Index extends Component {
 
-		constructor(props){
+
+	constructor(props) {
 		super(props);
-		this.state={
-			active:1,
+		var a = window.location.href.match(/\w+$/)
+		window.history.pushState({}, '', '/')
+		if (a) {
+			a = a.toString().toLowerCase();
+
+		}
+		else {
+			a = ''
+		}
+		this.state = {
+			active: a,
 		}
 	}
-	
-	homeClicked(){
-		this.setState({active:1,});
+
+	homeClicked() {
+		this.setState({ active: '', });
 	}
-	blogClicked(){
-		this.setState({active:2,});
+	blogClicked() {
+		this.setState({ active: 'blog', });
 	}
-	contClicked(){
-		this.setState({active:3,});
+	contClicked() {
+		this.setState({ active: 'contact', });
 	}
-	aboutClicked(){
-		this.setState({active:4,});
+	aboutClicked() {
+		this.setState({ active: 'about', });
 	}
-	recruitmentClicked(){
-		this.setState({active:5,});
+	recruitmentClicked() {
+		this.setState({ active: 'recruitment', });
+	}
+	eventsClicked() {
+		this.setState({ active: 'events', });
 	}
 
-	render(){
-		return(
-			<Router>
+	render() {
+		return (
+			< Router >
 				<div>
-					<header id="header">      
+					<h1>{ }</h1>
+					<header id="header">
 						<div className="container">
 							<div className="row">
 								<div className="col-sm-12 overflow">
-								<div className="social-icons pull-right">
+									<div className="social-icons pull-right">
 										<ul className="nav nav-pills">
 											<li><a href="https://www.facebook.com/AWSCloudCommunityLPU"><i className="fa fa-facebook"></i></a></li>
 											<li><a href="https://www.instagram.com/awscloudcommunity_lpu/"><i className="fa fa-instagram"></i></a></li>
 											<li><a href="https://www.youtube.com/channel/UCmnQggVDZY8ht6UYekmzQlg/featured"><i className="fa fa-youtube"></i></a></li>
 											<li><a href="https://www.linkedin.com/company/aws-cloud-community-lpu/"><i className="fa fa-linkedin"></i></a></li>
 										</ul>
-									</div> 
+									</div>
 								</div>
 							</div>
 						</div>
@@ -65,45 +80,41 @@ class Index extends Component {
 										<span className="icon-bar"></span>
 									</button>
 
-									
-									<Link to="/index.html " className="navbar-brand">
-									<h1><img src="images/logo.png" alt="logo" /></h1>
-									</Link> 
-										
-									
-									
+
+									<Link to="/" className="navbar-brand">
+										<h1><img src="images/logo.png" alt="logo" /></h1>
+									</Link>
+
+
+
 								</div>
 								<div className="collapse navbar-collapse">
 									<ul className="nav navbar-nav navbar-right">
-										<li className={this.state.active==1 && "active"} onClick={() => {this.homeClicked()}}><Link to="/">Home</Link></li>
-															
-										<li className={this.state.active==2 && "active"} onClick={() => {this.blogClicked()}}><Link to="/Blog">Blog </Link>
-											
-										</li>
-										<li className={this.state.active==3 && "active"} onClick={() => {this.contClicked()}}><Link to="/Contact">Contact Us</Link>
-
-										</li>                         
-
-										<li className={this.state.active==4 && "active"} onClick={() => {this.aboutClicked()}}><Link to ="/About">About Us</Link></li>  
-										<li className={this.state.active==5 && "active"} onClick={() => {this.recruitmentClicked()}}><Link to ="/Recruitment">Recruitment</Link></li>                  
+										<li className={this.state.active == '' && "active"} onClick={() => { this.homeClicked(); window.location.reload() }}><Link to="/">Home</Link></li>
+										<li className={this.state.active == 'blog' && "active"} onClick={() => { this.blogClicked() }}><Link to="/Blog">Blog </Link></li>
+										<li className={this.state.active == 'contact' && "active"} onClick={() => { this.contClicked() }}><Link to="/Contact">Contact Us</Link></li>
+										<li className={this.state.active == 'about' && "active"} onClick={() => { this.aboutClicked() }}><Link to="/About">About Us</Link></li>
+										<li className={this.state.active == 'recruitment' && "active"} onClick={() => { this.recruitmentClicked() }}><Link to="/Recruitment">Recruitment</Link></li>
+										<li className={this.state.active == 'events' && "active"} onClick={() => { this.eventsClicked() }}><Link to="/Events">Events</Link></li>
 
 									</ul>
 								</div>
-								
+
 							</div>
 						</div>
 					</header>
 
-				
+
 					<Route path="/" exact component={Home} />
 					<Route path="/contact/" component={Contact} />
 					<Route path="/about/" component={About} />
 					<Route path="/blog/" component={Blog} />
 					<Route path="/recruitment/" component={Recruitment} />
+					<Route path="/events/" component={Events} />
 
-					<Footer/>
+					<Footer />
 				</div>
-			</Router>
+			</Router >
 		)
 	}
 }
